fa6f1af3b0b4a9cd5acde11d9706acf69b0c0d3d
Ranran He <cangxuexiao@gmail.com>
2019-10-14 23:35:23 -0400

    edit title

---
+++ js/fonts.js
@@ -0,0 +1,85 @@
+import {isBold} from "./bold.js";
+import {isItalics} from "./italic.js";
+
+export function insertHtmlAtCaret(style) {
+    let sel, range;
+    if (window.getSelection()) {
+        sel = window.getSelection();
+        if (sel.getRangeAt && sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            const el = document.createElement("div");
+
+            const span = document.createElement('div');
+            span.append(range.cloneContents());
+            // console.log(span);
+
+            if (style === 'B') {
+                removeClass(span, "bold_false");
+                addClass(span, 'bold');
+                el.innerHTML = '<span class="bold">' + span.innerHTML + '</span>';
+            }
+            if (style === 'NB') {
+                el.innerHTML = '<span class="bold_false">' + sel + '</span>';
+            }
+            if (style === 'I') {
+                removeClass(span, "italic_false");
+                addClass(span, 'italic');
+                // el.innerHTML = '<span>' + span.innerHTML + '</span>';
+                // el.style.fontStyle = 'italic';
+                el.innerHTML = '<span class="italic">' + span.innerHTML + '</span>';
+            }
+            if (style === 'NI') {
+                removeClass(span, "italic");
+                if (isBold()) {
+                    console.log("italic!!")
+                    el.innerHTML = '<span class="italic_false">' + sel + '</span>';
+                } else {
+                    el.innerHTML = '<span class="italic_false">' + sel + '</span>';
+                }
+            }
+            range.deleteContents();
+            var frag = document.createDocumentFragment(), node, lastNode;
+            while ((node = el.firstChild)) {
+                lastNode = frag.appendChild(node);
+            }
+            range.insertNode(frag);
+            if (lastNode) {
+                range = range.cloneRange();
+                range.setStartAfter(lastNode);
+                range.collapse(true);
+                // sel.removeAllRanges();
+                // sel.addRange(range);
+            }
+            console.log(document.getElementById('text').innerHTML);
+        }
+    }
+}
+
+function removeClass(element, style) {
+    if (element.childElementCount === 0) {
+        return;
+    }
+    element.childNodes.forEach(node => {
+        if (node.className !== undefined) {
+            if (node.classList.contains(style)) {
+                node.classList.remove(style);
+            }
+        }
+        removeClass(node);
+    })
+}
+
+function addClass(element, style) {
+    if (element.childElementCount === 0) {
+        return;
+    }
+    element.childNodes.forEach(node => {
+        if (node.className !== undefined) {
+            if (!node.classList.contains(style)) {
+                node.classList.add(style);
+            }
+        }
+        removeClass(node);
+    })
+}
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { isBold } from './bold.js';
 import { isItalics } from "./italic.js";
+import { insertHtmlAtCaret } from "./fonts.js";
 
 document.onkeyup = document.onmouseup = function () {
     // console.log(window.getSelection().anchorNode.parentElement.tagName)
@@ -31,6 +32,11 @@ function buttonItalicsColor() {
 }
 
 
+
+
+
+
+
 document.getElementById('btn_bold').onclick = function () {
     document.getElementById('text').focus();
     if (isBold())
@@ -47,80 +53,6 @@ document.getElementById('btn_italics').onclick = function () {
         insertHtmlAtCaret('I');
 };
 
-function removeClass(element, style) {
-    if (element.childElementCount === 0) {
-        return;
-    }
-    element.childNodes.forEach(node => {
-        if (node.className !== undefined) {
-            if (node.classList.contains(style)) {
-                node.classList.remove(style);
-            }
-        }
-        removeClass(node);
-    })
-}
 
-function addClass(element, style) {
-    if (element.childElementCount === 0) {
-        return;
-    }
-    element.childNodes.forEach(node => {
-        if (node.className !== undefined) {
-            if (!node.classList.contains(style)) {
-                node.classList.add(style);
-            }
-        }
-        removeClass(node);
-    })
-}
-
-function insertHtmlAtCaret(style) {
-    var sel, range;
-    if (window.getSelection()) {
-        sel = window.getSelection();
-        if (sel.getRangeAt && sel.rangeCount) {
-            range = sel.getRangeAt(0);
-            var el = document.createElement("div");
-
-            var span = document.createElement('div');
-            span.append(range.cloneContents());
-            // console.log(span);
-
-            if (style === 'B') {
-                removeClass(span, "bold_false");
-                addClass(span, 'bold');
-                el.innerHTML = '<span class="bold">' + span.innerHTML + '</span>';
-            }
-            if (style === 'NB') {
-                el.innerHTML = '<span class="bold_false">' + sel + '</span>';
-            }
-            if (style === 'I') {
-                removeClass(span, "italic_false");
-                addClass(span, 'italic');
-                // el.innerHTML = '<span>' + span.innerHTML + '</span>';
-                // el.style.fontStyle = 'italic';
-                el.innerHTML = '<span class="italic">' + span.innerHTML + '</span>';
-            }
-            if (style === 'NI') {
-                el.innerHTML = '<span class="italic_false">' + sel + '</span>';
-            }
-            range.deleteContents();
-            var frag = document.createDocumentFragment(), node, lastNode;
-            while ((node = el.firstChild)) {
-                lastNode = frag.appendChild(node);
-            }
-            range.insertNode(frag);
-            if (lastNode) {
-                range = range.cloneRange();
-                range.setStartAfter(lastNode);
-                range.collapse(true);
-                // sel.removeAllRanges();
-                // sel.addRange(range);
-            }
-            console.log(document.getElementById('text').innerHTML);
-        }
-    }
-}
 
 
