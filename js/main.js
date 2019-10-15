8ed1879d19cbf3e1a752e7a05fc38b0d2a63e7b1
Ranran He <cangxuexiao@gmail.com>
2019-10-14 22:37:11 -0400

    fixed bold rules

--- js/main.js
+++ js/main.js
@@ -33,6 +33,72 @@ function isBold() {
     el.append(range.cloneContents());
 
     const ancestorBold = function () {
+        console.log(el);
+        console.log(el.childElementCount);
+        if (range.commonAncestorContainer.classList === undefined || range.commonAncestorContainer.classList.value === "") {
+            if (el.childNodes.length === 1 && el.childElementCount === 0) return true;
+            if (el.childElementCount < el.childNodes.length) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        else return !range.commonAncestorContainer.classList.contains('bold_false');
+    };
+
+    var bold = true;
+    AllBold(el);
+    function AllBold(ele) {
+        console.log(ele)
+        if (ele.childElementCount !== 0) {
+            for(let i = 0; i < ele.childNodes.length; i ++) {
+                if (ele.childNodes[i].tagName !== undefined) {
+                    console.log(ele.childNodes[i].classList.contains('bold_false'))
+                    console.log(ele.childNodes[i].childNodes.length > ele.childNodes[i].childElementCount)
+                    console.log(ele.childNodes[i].childElementCount)
+                    if (ele.childNodes[i].classList.contains('bold_false') && ele.childNodes[i].childNodes.length > ele.childNodes[i].childElementCount) {
+                        console.log("false here")
+                        bold = false;
+                    }
+                    if (ele.childNodes[i].parentElement.style.fontWeight === '400') {
+                        console.log("false herhhhhhh")
+                        bold = false;
+                    }
+                    AllBold(ele.childNodes[i]);
+                }
+            }
+            // ele.childNodes.every(node => {
+            //     console.log(node)
+            //     if (node.tagName !== undefined) {
+            //         if (node.parentElement.style.fontWeight === '400') return false;
+            //         if (node.classList.contains('bold_false') && node.childNodes.length > node.childElementCount) {
+            //             console.log("false here")
+            //             return false;
+            //         }
+            //         AllBold(node);
+            //     }
+            // });
+        }
+        // return bool;
+    };
+
+    console.log(getComputedStyle(window.getSelection().anchorNode.parentElement).fontWeight !== '400');
+    console.log(getComputedStyle(window.getSelection().focusNode.parentElement).fontWeight !== '400');
+    console.log(bold);
+    console.log(ancestorBold() === true);
+
+    return (getComputedStyle(window.getSelection().anchorNode.parentElement).fontWeight !== '400'
+        && getComputedStyle(window.getSelection().focusNode.parentElement).fontWeight !== '400'
+        && bold && ancestorBold())
+}
+
+function isItalics() {
+    const range = window.getSelection().getRangeAt(0);
+    const el = document.createElement("div");
+    el.append(range.cloneContents());
+
+    const ancestorBold = function () {
+        // console.log(range.commonAncestorContainer.classList);
         // console.log(el.childNodes);
         // console.log(el.childElementCount);
         if (range.commonAncestorContainer.classList === undefined || range.commonAncestorContainer.classList.value === "") {
@@ -44,7 +110,7 @@ function isBold() {
                 return true;
             }
         }
-        else return !range.commonAncestorContainer.classList.contains('bold_false');
+        else return !range.commonAncestorContainer.classList.contains('italic_false');
     };
 
     const AllBold = function (ele, bool) {
@@ -52,55 +118,6 @@ function isBold() {
         if (ele.childElementCount !== 0) {
             ele.childNodes.forEach(node => {
                 // console.log(node)
-                if (node.tagName !== undefined) {
-                    bool = bool && node.parentElement.style.fontWeight !== '400';
-                    if (node.classList.contains('bold_false') && node.childNodes.length > node.childElementCount) {
-                        bool = false;
-                    }
-                    AllBold(node, bool);
-                }
-            });
-        }
-        return bool;
-    };
-
-    // console.log(getComputedStyle(window.getSelection().anchorNode.parentElement).fontWeight !== '400');
-    // console.log(getComputedStyle(window.getSelection().focusNode.parentElement).fontWeight !== '400');
-    // console.log(AllBold(el, true) === true);
-    // console.log(ancestorBold() === true);
-
-    return (getComputedStyle(window.getSelection().anchorNode.parentElement).fontWeight !== '400'
-        && getComputedStyle(window.getSelection().focusNode.parentElement).fontWeight !== '400'
-        && AllBold(el, true) && ancestorBold())
-}
-
-function isItalics() {
-    const range = window.getSelection().getRangeAt(0);
-    const el = document.createElement("div");
-    el.append(range.cloneContents());
-
-    const ancestorBold = function () {
-        console.log(range.commonAncestorContainer.classList
-        );
-        console.log(el.childNodes);
-        console.log(el.childElementCount);
-        if (range.commonAncestorContainer.classList === undefined || range.commonAncestorContainer.classList.value === "") {
-            console.log("here!!!!");
-            if (el.childNodes.length === 1 && el.childElementCount === 0) return true;
-            if (el.childElementCount < el.childNodes.length) {
-                return false;
-            } else {
-                return true;
-            }
-        }
-        else return !range.commonAncestorContainer.classList.contains('italic_false');
-    };
-
-    const AllBold = function (ele, bool) {
-        console.log(ele)
-        if (ele.childElementCount !== 0) {
-            ele.childNodes.forEach(node => {
-                console.log(node)
                 if (node.tagName !== undefined) {
                     bool = bool && (node.parentElement.style.fontStyle === 'italic' || node.parentElement.style.fontStyle === '');
                     if (node.classList.contains('italic_false') && node.childNodes.length > node.childElementCount) {
@@ -113,10 +130,10 @@ function isItalics() {
         return bool;
     };
 
-    console.log(getComputedStyle(window.getSelection().anchorNode.parentElement).fontStyle === 'italic');
-    console.log(getComputedStyle(window.getSelection().focusNode.parentElement).fontStyle === 'italic');
-    console.log(AllBold(el, true) === true);
-    console.log(ancestorBold() === true);
+    // console.log(getComputedStyle(window.getSelection().anchorNode.parentElement).fontStyle === 'italic');
+    // console.log(getComputedStyle(window.getSelection().focusNode.parentElement).fontStyle === 'italic');
+    // console.log(AllBold(el, true) === true);
+    // console.log(ancestorBold() === true);
 
     return (getComputedStyle(window.getSelection().anchorNode.parentElement).fontStyle === 'italic'
         && getComputedStyle(window.getSelection().focusNode.parentElement).fontStyle === 'italic'
